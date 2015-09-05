358f618e0474247599fcd7c73430f8715da23a2b
Kristian Øllegaard <kristian@kristian.io>
2015-09-05 13:05:00 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='hotel-elasticsearch',
-    version='0.1.1',
+    version='0.2.0',
     packages=['hotel_elasticsearch'],
     url='',
     license='',
