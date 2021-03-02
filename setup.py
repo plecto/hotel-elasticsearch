880f60c1d3ba95e8ce85706e50d9a3ee055b6e2f
Kristian Øllegaard <kristian@oellegaard.com>
2021-03-02 21:18:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='hotel-elasticsearch',
-    version='0.2.0',
+    version='1.0.0',
     packages=['hotel_elasticsearch'],
     url='',
     license='',
