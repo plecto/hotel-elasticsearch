523f6481bb7d25c4c015abb829d5ddb5080473a3
Sune Kjærgård <4482470+SuneKjaergaard@users.noreply.github.com>
2023-08-30 12:54:15 +0200

    Fix existing tests and add a new one for initial master

--- hotel_elasticsearch/tests/test_configuration.py
+++ hotel_elasticsearch/tests/test_configuration.py
@@ -1,6 +1,8 @@
 from tempfile import NamedTemporaryFile
 from unittest import TestCase
-from hotel_elasticsearch.cluster import Cluster
+from unittest.mock import PropertyMock, patch
+
+from hotel_elasticsearch.clusternode import ClusterNode
 from hotel_elasticsearch.configuration import ElasticSearchConfig
 import yaml
 
@@ -15,8 +17,7 @@ node:
   availability_zone: \${EC2_AZ}
   instance_id: \${EC2_INSTANCE_ID}
   name: test_node
-  data: false
-  master: true
+  roles: ['master']
 path:
   data: /mnt/elasticsearch/
   logs: /var/log/elasticsearch
@@ -28,35 +29,42 @@ routing:
 
     def setUp(self):
         self.config_file_instance = NamedTemporaryFile()
-        self.config_file_instance.write(self.initial_data)
+        self.config_file_instance.write(self.initial_data.encode('utf-8'))
         self.config_file_instance.seek(0)
 
     def test_initial_file(self):
-        config = ElasticSearchConfig(Cluster("elasticsearch-testcase-master-h0slave-r01"), config_file=self.config_file_instance.name)
+        config = ElasticSearchConfig(ClusterNode("elasticsearch-testcase-master-h0slave-r01"), config_file=self.config_file_instance.name)
         config.save()
         # self.assertEqual(self.initial_data, self.config_file_instance.read())
-        self.assertDictEqual(yaml.load(self.initial_data), yaml.load(self.config_file_instance.read()))
+        self.assertDictEqual(yaml.safe_load(self.initial_data), yaml.safe_load(self.config_file_instance.read().decode('utf-8')))
         self.config_file_instance.close()
 
     def test_client_node(self):
-        self.initial_data = self.initial_data.replace("data: false", "data: false")
-        self.initial_data = self.initial_data.replace("master: true", "master: false")
-        config = ElasticSearchConfig(Cluster("elasticsearch-testcase-client-h0slave-r01"), config_file=self.config_file_instance.name)
+        self.initial_data = self.initial_data.replace("['master']", "[]")
+        config = ElasticSearchConfig(ClusterNode("elasticsearch-testcase-client-h0slave-r01"), config_file=self.config_file_instance.name)
         config.save()
-        self.assertDictEqual(yaml.load(self.initial_data), yaml.load(self.config_file_instance.read()))
+        self.assertDictEqual(yaml.safe_load(self.initial_data), yaml.safe_load(self.config_file_instance.read()))
         self.config_file_instance.close()
 
     def test_master_node(self):
-        self.initial_data = self.initial_data.replace("data: false", "data: false")
-        config = ElasticSearchConfig(Cluster("elasticsearch-testcase-master-h0slave-r01"), config_file=self.config_file_instance.name)
+        config = ElasticSearchConfig(ClusterNode("elasticsearch-testcase-master-h0slave-r01"), config_file=self.config_file_instance.name)
         config.save()
-        self.assertDictEqual(yaml.load(self.initial_data), yaml.load(self.config_file_instance.read()))
+        self.assertDictEqual(yaml.safe_load(self.initial_data), yaml.safe_load(self.config_file_instance.read()))
+        self.config_file_instance.close()
+
+    @patch('hotel_elasticsearch.clusternode.ClusterNode.tags', {'initial_master': 'True'})
+    @patch('hotel_elasticsearch.clusternode.ClusterNode.instance_id', "i-1234567890")
+    def test_initial_master_node(self):
+        config = ElasticSearchConfig(ClusterNode("elasticsearch-testcase-master-h0slave-r01"), config_file=self.config_file_instance.name)
+        config.save()
+        reference_config = yaml.safe_load(self.initial_data)
+        reference_config['cluster']['initial_master_nodes'] = ['i-1234567890']
+        self.assertDictEqual(reference_config, yaml.safe_load(self.config_file_instance.read()))
         self.config_file_instance.close()
 
     def test_data_node(self):
-        self.initial_data = self.initial_data.replace("master: true", "master: false")
-        self.initial_data = self.initial_data.replace("data: false", "data: true")
-        config = ElasticSearchConfig(Cluster("elasticsearch-testcase-data-h0slave-r01"), config_file=self.config_file_instance.name)
+        self.initial_data = self.initial_data.replace("['master']", "['data']")
+        config = ElasticSearchConfig(ClusterNode("elasticsearch-testcase-data-h0slave-r01"), config_file=self.config_file_instance.name)
         config.save()
-        self.assertDictEqual(yaml.load(self.initial_data), yaml.load(self.config_file_instance.read()))
+        self.assertDictEqual(yaml.safe_load(self.initial_data), yaml.safe_load(self.config_file_instance.read()))
         self.config_file_instance.close()
