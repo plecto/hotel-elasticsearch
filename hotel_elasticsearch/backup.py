24a8e0cb586b37c314910ff65308b9e82fcf85ff
Sune Kjærgård <4482470+SuneKjaergaard@users.noreply.github.com>
2023-08-30 12:45:41 +0200

    Newer versions of Elasticsearch supports snapshot life cycle management. So we don't need to manage taking snapshots in hotel-elasticsearch
    This also mean we can get rid of the distributed lock and it's dynamodb dependency

--- hotel_elasticsearch/backup.py
+++ hotel_elasticsearch/backup.py
@@ -9,28 +9,20 @@ import logging
 logger = logging.getLogger('hotel_elasticsearch.backup')
 
 class BackupManager(object):
-    def run_backup(self):
-        now = datetime.datetime.now()
-        snapshot_name = 'snapshot_%s-%s-%s_%s' % (now.year, now.month, now.day, now.hour)
-        logger.info("Executing backup command, snapshot name: %s" % snapshot_name)
-        r = requests.put("http://localhost:9200/_snapshot/%s/%s/" % (
-            'hotel-backup',
-            snapshot_name
-        ))
-        r.raise_for_status()
-        logger.info("Snapshot successfully scheduled")
+    def check_backup(self):
+        pass
+
+    def restore_backup(self):
+        pass
+
+    def configure_backup(self):
+        pass
 
 
 def backup_thread():
+    bm = BackupManager()
+
     while True:
-        try:
-            with Timer('es_backup', days=1) as t:
-                bm = BackupManager()
-                try:
-                    bm.run_backup()
-                except HTTPError as e:
-                    logger.exception(e)
-                    t.clear_timer()  # If backup failed, try again on this or another node
-        except BlockedByAnotherTimerException:  # Backup has already been run, let it pass
-            pass
-        time.sleep(random.randrange(60, 120))
+        time.sleep(random.randrange(5*60, 10*60))
+        bm.check_backup()
+
