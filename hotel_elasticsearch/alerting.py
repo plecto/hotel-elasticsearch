e46796d70d708dee87176ee6d4c3e7619e6d874a
Sune Kjærgård <4482470+SuneKjaergaard@users.noreply.github.com>
2023-09-14 12:53:32 +0200

    No need for a Mixin

--- hotel_elasticsearch/alerting.py
+++ hotel_elasticsearch/alerting.py
@@ -11,14 +11,6 @@ from hotel_elasticsearch.configuration import HotelElasticSearchConfig
 
 logger = logging.getLogger('hotel_elasticsearch.cluster_node')
 
-class AWSSecretsMixin(object):
-    @staticmethod
-    def get_secret(secret_name):
-        client = botocore_client_factory('secretsmanager')
-        cache_config = SecretCacheConfig()  # Initializes an LRU cache on the thread
-        cache = SecretCache(config=cache_config, client=client)
-        return cache.get_secret_string(secret_name)
-
 
 class BaseAlerter(object):
     def __init__(self, config: dict, cluster_node: ClusterNode):
@@ -41,7 +33,7 @@ class NoopAlerter(BaseAlerter):
         pass
 
 
-class PagerDutyAlerter(AWSSecretsMixin, BaseAlerter):
+class PagerDutyAlerter(BaseAlerter):
     def validate_config(self):
         assert 'pagerduty' in self.config
         assert 'type' in self.config['pagerduty']
@@ -52,7 +44,7 @@ class PagerDutyAlerter(AWSSecretsMixin, BaseAlerter):
         result = requests.post(
             'https://events.pagerduty.com/v2/enqueue',
             json={
-                'routing_key': self.get_secret(self.config['pagerduty']['secret_name']),
+                'routing_key': get_aws_secret(self.config['pagerduty']['secret_name']),
                 'event_action': 'trigger',
                 'payload': {
                     'summary': message,
@@ -74,3 +66,10 @@ def alerter_factory(cluster_node):
         return PagerDutyAlerter(config['hotel']['alerting'], cluster_node)
     else:
         return NoopAlerter({}, cluster_node)
+
+
+def get_aws_secret(secret_name):
+    client = botocore_client_factory('secretsmanager')
+    cache_config = SecretCacheConfig()  # Initializes an LRU cache on the thread
+    cache = SecretCache(config=cache_config, client=client)
+    return cache.get_secret_string(secret_name)
